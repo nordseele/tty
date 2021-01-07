7fbf15b3b11d5c8d7734671fbbe6c4b0caabda2a
Matthias Budde <matt@nordseele.com>
2021-01-07 12:18:54 +0000

    readme

--- src/ttymidi.c
+++ src/ttymidi.c
@@ -458,7 +458,7 @@ void* read_midi_from_serial_port(void* seq)
 				/* Status byte received */
 				buf[0] = buf[i];
 
-				if (buf[0] == 0xFA || buf[0] == 0xFB || buf[0] == 0xF8 || buf[0] == 0xFC) { // dirty test if buf[0] is a one byte beatclock msg
+				if (buf[0] == 0xFA || buf[0] == 0xFB || buf[0] == 0xF8 || buf[0] == 0xFC) { // is buf[0] a one byte beatclock msg ?
 					i = 3;
 				} else {
 					i = 1;
